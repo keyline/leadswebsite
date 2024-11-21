2e3dc22970bea37510ddb2a085d0c372e4285dfa
Deblina Das <deblina@keylines.net>
2024-11-21 12:42:55 +0530

    leads product pegination isuue fixing

--- app/Views/front/pages/product-list.php
+++ app/Views/front/pages/product-list.php
@@ -120,21 +120,7 @@
                         repeatCount="indefinite"
                         begin="0.3" />
                 </circle>
-            </svg>
-
-            <!-- <svg version="1.1" id="L9" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                viewBox="0 0 100 100" enable-background="new 0 0 0 0" xml:space="preserve">
-                <path fill="#ed1c24" d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50">
-                    <animateTransform
-                        attributeName="transform"
-                        attributeType="XML"
-                        type="rotate"
-                        dur="1s"
-                        from="0 50 50"
-                        to="360 50 50"
-                        repeatCount="indefinite" />
-                </path>
-            </svg> -->
+            </svg>            
 
         </div>
 
@@ -151,96 +137,6 @@
 <!-- feature icon section end -->
 
 <?= $this->section('scripts') ?>
-<!-- <script>
-    $(document).ready(function() {
-        // _________________________ load more _________________________
-        let page = 1;
-        let loading = false;
-
-        function loadMoreContent() {
-            if (!loading) {
-                loading = true; // Set loading to true
-                $('#loading').show(); // Show loading indicator
-
-                page++; // Increment the page number
-                $.ajax({
-                    url: 'api/load_more_products', // URL to load more content
-                    type: 'GET', // HTTP method
-                    data: {
-                        page: page
-                    }, // Pass the page number to the server
-                    success: function(response) {
-
-                        if (response.status) {
-                            $('#product_list').append(response.html); // Append new content
-                        } else {
-                            $('#load_more_btn').hide(); // Hide the button if no more content
-                        }
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        console.error('Error loading Products:', textStatus, errorThrown);
-                    },
-                    complete: function() {
-                        loading = false; // Reset loading flag
-                        $('#loading').hide(); // Hide loading indicator
-                    }
-                });
-            }
-        }
-
-        $('#load_more_btn').on('click', loadMoreContent);
-
-
-
-
-
-
-
-        var a = new StickySidebar('#sticky-sidebar-demo', {
-            topSpacing: 25,
-            containerSelector: '.blogdetails_item',
-            innerWrapperSelector: '.sidebar__inner'
-        });
-
-        var a = new StickySidebar('#sticky-sidebar-cateogy', {
-            topSpacing: 25,
-            containerSelector: '.blogdetails_item',
-            innerWrapperSelector: '.sidebar__inner'
-        });
-
-
-        $("#blogdetails-recent").owlCarousel({
-            loop: true,
-            margin: 20,
-            dots: false,
-            nav: false,
-            autoplay: false,
-            autoplayTimeout: 4000,
-            autoplayHoverPause: true,
-            animateIn: 'fadeIn',
-            animateOut: 'fadeOut',
-            navText: ["<i class='zmdi zmdi-arrow-left'></i>", "<i class='zmdi zmdi-arrow-right'></i>"],
-            responsive: {
-                0: {
-                    items: 1,
-                },
-                600: {
-                    items: 2,
-                },
-                750: {
-                    items: 2,
-                },
-                1000: {
-                    items: 3,
-                },
-                1200: {
-                    items: 4,
-                }
-            }
-        });
-
-    });
-</script> -->
 
 <script>
     let offset = <?= count($product) ?>; // Track how many products have been loaded
@@ -315,7 +211,7 @@
                     $('#loading').hide();
                     $('#load_more_btn').hide(); // Hide button if no more products
                     if ($('#no_more_products').length === 0) {
-                    $('#product_list').after('<p id="no_more_products" class="text-center">No more products to load.</p>');
+                    $('#product_list').after('<p id="no_more_products" class="background-color:#ed1c24;border: tomato;">No more products to load.</p>');
                 }
                     
                 }
