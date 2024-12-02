104ee3450f2fe95e8219cb3158ced8e8d7ec55d0
Deblina Das <deblina@keylines.net>
2024-12-02 12:55:37 +0530

    distributor form submition

--- app/Views/admin/maincontents/enquire/list.php
+++ app/Views/admin/maincontents/enquire/list.php
@@ -71,6 +71,7 @@ $isDistributorEnquiry = ($moduleDetail['controller'] == 'manage_distributor_enqu
                                 if ($rows) {                                    
                                     echo $apikey1 = array_search(2, array_column($productcat, 'id')) ;
                                     pr($productcat);
+                                    echo $productcat[$apikey1]['name'];
                                     $i = 1;
                                     foreach ($rows as $row) {
                                 ?>
