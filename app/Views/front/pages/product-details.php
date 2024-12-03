d7b9f338fec9eee4362de56902623b3be267def2
debojyti <debojyti@keylines.net>
2024-12-03 14:39:26 +0530

    lead031224-4

--- app/Views/front/pages/product-details.php
+++ app/Views/front/pages/product-details.php
@@ -96,9 +96,9 @@
                             <div class="product_dtl_info">
                             <h2><?=$product->product_title; ?> <?php if($product->is_new == 1){ ?><a href="#" class="new_badge">New</a><?php }?> </h2>
                                 <div class="offerprice_top">
-                                    <h5>Offer Price: </h5>
+                                    <h5>Offer Price</h5>
                                     <?php if(!empty($product->regular_price)){ ?>                                  
-                                    <div class="pricename_offer"> <span class="regular_price"><span style="padding-right: 5px;font-size: 18px;"><i class="fa-solid fa-indian-rupee-sign"></i></span><?=$product->regular_price?></span></div> 
+                                    <div class="pricename_offer"><span class="spancolon" style="font-weight: normal;">:</span> <span class="regular_price"><span style="padding-right: 5px;font-size: 18px;"><i class="fa-solid fa-indian-rupee-sign"></i></span><?=$product->regular_price?></span></div> 
                                     <?php }?>
                                 </div>
                             
