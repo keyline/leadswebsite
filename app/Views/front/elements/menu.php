eeb81878d0254280a19c9b90fe3bb6c647523081
Deblina Das <deblina@keylines.net>
2024-12-03 15:54:28 +0530

    remove key feature required

--- app/Views/front/elements/menu.php
+++ app/Views/front/elements/menu.php
@@ -88,6 +88,8 @@
                         <span class="without_melink">After Sales Sevices</span>
                         <ul>
                             <li><a href="<?= base_url('/amc-policy') ?>">AMC</a></li>
+                            <li><a href="<?= base_url('/service') ?>">Enquiry & Service Request</a></li>
+                            <li><a href="<?= base_url('/return-policy') ?>">Return Policy</a></li>
                             <!-- <li><a href="#">Service Policy</a></li> -->
                             <?php foreach ($contents as $key => $page) { ?>
                                 <li><a href="<?= base_url() ?>/page/<?= $page->slug ?>"><?= $page->title ?></a></li>
